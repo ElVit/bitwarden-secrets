eb1e668cc40235840777e2b74580a5b8bdbcb816
ElVit <54866762+ElVit@users.noreply.github.com>
2025-03-24 21:57:03 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,23 +13,10 @@ export NODE_OPTIONS="--no-deprecation"
 readonly __COLORS_ESCAPE="\033[";
 readonly __COLORS_RESET="${__COLORS_ESCAPE}0m"
 readonly __COLORS_DEFAULT="${__COLORS_ESCAPE}39m"
-readonly __COLORS_BLACK="${__COLORS_ESCAPE}30m"
 readonly __COLORS_RED="${__COLORS_ESCAPE}31m"
 readonly __COLORS_GREEN="${__COLORS_ESCAPE}32m"
 readonly __COLORS_YELLOW="${__COLORS_ESCAPE}33m"
 readonly __COLORS_BLUE="${__COLORS_ESCAPE}34m"
-readonly __COLORS_MAGENTA="${__COLORS_ESCAPE}35m"
-readonly __COLORS_CYAN="${__COLORS_ESCAPE}36m"
-readonly __COLORS_LIGHT_GRAY="${__COLORS_ESCAPE}37m"
-readonly __COLORS_BG_DEFAULT="${__COLORS_ESCAPE}49m"
-readonly __COLORS_BG_BLACK="${__COLORS_ESCAPE}40m"
-readonly __COLORS_BG_RED="${__COLORS_ESCAPE}41m"
-readonly __COLORS_BG_GREEN="${__COLORS_ESCAPE}42m"
-readonly __COLORS_BG_YELLOW="${__COLORS_ESCAPE}43m"
-readonly __COLORS_BG_BLUE="${__COLORS_ESCAPE}44m"
-readonly __COLORS_BG_MAGENTA="${__COLORS_ESCAPE}45m"
-readonly __COLORS_BG_CYAN="${__COLORS_ESCAPE}46m"
-readonly __COLORS_BG_WHITE="${__COLORS_ESCAPE}47m"
 
 #
 # Script functions
@@ -37,7 +24,7 @@ readonly __COLORS_BG_WHITE="${__COLORS_ESCAPE}47m"
 log.white()
 {
   local message=$*
-  echo -e "${message}" >&2
+  echo -e "${__COLORS_DEFAULT}${message}${__COLORS_RESET}" >&2
   return 0
 }
 
@@ -62,12 +49,19 @@ log.yellow()
   return 0
 }
 
+log.blue()
+{
+  local message=$*
+  echo -e "${__COLORS_BLUE}${message}${__COLORS_RESET}" >&2
+  return 0
+}
+
 login()
 {
-  log.white "Configuring Bitwarden server..."
+  log.white "Configuring bitwarden server ..."
   bw config server ${BW_SERVER} &>/dev/null
 
-  log.white "Logging into Bitwarden..."
+  log.white "Logging into bitwarden ..."
   SESSION=$(bw login --raw ${BW_USERNAME} ${BW_PASSWORD}) &>/dev/null
 
   if [ $? -eq 0 ]; then
@@ -75,7 +69,7 @@ login()
     export BW_SESSION=${SESSION}
   else
     echo ""
-    log.red "Bitwarden login failed. Exiting..."
+    log.red "Bitwarden login failed. Exiting ..."
     exit 1
   fi
 }
@@ -88,7 +82,7 @@ logout()
 
   # Logout and ignore possible errors
   bw logout &>/dev/null
-  log.white "Logged out of Bitwarden."
+  log.white "Logged out of bitwarden."
 }
 
 login_check()
@@ -96,23 +90,23 @@ login_check()
   bw login --check &>/dev/null
 
   if [ $? -eq 0 ]; then
-    log.white "Logged in to Bitwarden"
+    log.white "Logged in to bitwarden."
   else
-    log.yellow "Bitwarden login expired. Logging in again..."
+    log.yellow "Bitwarden login expired. Logging in again ..."
     login
   fi
 }
 
 set_org_id()
 {
-  log.white "Retrieving organization id..."
+  log.white "Retrieving organization id ..."
   ORG=$(bw get organization "${BW_ORGANIZATION}" | jq -r '.id') 2>/dev/null
 
   if [ $? -eq 0 ]; then
     log.white "Retrieved organization id for ${BW_ORGANIZATION}"
     export BW_ORG_ID=${ORG}
   else
-    log.red "Could not retrieve Bitwarden organization ${BW_ORGANIZATION}. Exiting..."
+    log.red "Could not retrieve bitwarden organization ${BW_ORGANIZATION}. Exiting ..."
     exit 1
   fi
 }
@@ -121,8 +115,8 @@ generate_secrets()
 {
   touch ${TEMP_SECRETS_FILE}
 
-  printf "# Home Assistant secrets file\n" >> ${TEMP_SECRETS_FILE}
-  printf "# DO NOT MODIFY -- Managed by Bitwarden Secrets for Home Assistant add-on\n" >> ${TEMP_SECRETS_FILE}
+  printf "# bitwarden secrets file\n" >> ${TEMP_SECRETS_FILE}
+  printf "# DO NOT MODIFY -- managed by bitwarden-secrets docker container\n" >> ${TEMP_SECRETS_FILE}
 
   for row in $(bw list items --organizationid ${BW_ORG_ID} | jq -c '.[] | select(.type == 1) | (.|@base64)'); do
     printf "\n" >> ${TEMP_SECRETS_FILE}
@@ -137,7 +131,7 @@ generate_secrets()
     write_uris "${name}" "${row_contents}"
     write_custom_fields "${name}" "${row_contents}"
 
-    log.white "ROW: ${row_contents}"
+    #log.blue "ROW: ${row_contents}"
   done
 }
 
@@ -164,11 +158,11 @@ write_field()
   field_name=${3}
   suffix=${4}
 
-  log.white "Parsing row ${row_contents}"
+  #log.blue "Parsing row ${row_contents}"
   field="$(echo ${row_contents} | jq -r ${field_name})"
 
   if [ "${field}" != "null" ]; then
-    log.white "Writing ${secret_name}_${suffix} with ${field}"
+    #log.blue "Writing ${secret_name}_${suffix} with ${field}"
     echo "${secret_name}_${suffix}: '${field}'" >> ${TEMP_SECRETS_FILE}
   fi
 }
@@ -183,7 +177,7 @@ write_uris()
     for uris in $(echo ${row_contents} | jq -c '.login.uris | .[] | @base64' ); do
       uri=$(echo ${uris} | jq -r '@base64d' |  jq -r '.uri')
       if [ "${uri}" != "null" ]; then
-        log.white "Writing ${secret_name}_uri_${i} with ${uri}"
+        #log.blue "Writing ${secret_name}_uri_${i} with ${uri}"
         echo "${secret_name}_uri_${i}: '${uri}'" >> ${TEMP_SECRETS_FILE}
         ((i=i+1))
       fi
@@ -203,7 +197,7 @@ write_custom_fields()
       field_value=$(echo ${field_contents} | jq -r '.value')
 
       if [ "${field_name}" != "null" ] && [ "${field_value}" != "null" ]; then
-        log.white "Writing ${secret_name}_${field_name} with ${field_value}"
+        #log.blue "Writing ${secret_name}_${field_name} with ${field_value}"
         echo "${secret_name}_${field_name}: '${field_value}'" >> ${TEMP_SECRETS_FILE}
       fi
     done
@@ -252,7 +246,7 @@ mkdir -v -p $TEMP_SECRETS_DIR
 #
 # Start of main loop
 #
-log.white "Start retrieving your Home Assistant secrets from Bitwarden"
+log.white "Start retrieving your secrets from bitwarden ..."
 login
 set_org_id
 
@@ -260,21 +254,21 @@ while true; do
   num_of_items=$(bw list items --organizationid ${BW_ORG_ID} | jq length)
 
   if [ ${num_of_items} -gt 0 ]; then
-    log.white "Generating ${SECRETS_FILE} file from login entries..."
+    log.white "Generating ${SECRETS_FILE} file from login entries ..."
     generate_secrets
-    log.white "Home Assistant secrets generated."
+    log.white "Secrets file generated."
 
-    log.white "Comparing newly generated secrets to ${SECRETS_FILE}..."
+    log.white "Comparing newly generated secrets to ${SECRETS_FILE} ..."
     if cmp -s -- "${TEMP_SECRETS_FILE}" "${SECRETS_FILE}"; then
       rm -f ${TEMP_SECRETS_FILE}
       log.white "No secrets changes detected."
     else
-      log.white "Changes from Bitwarden detected, replacing ${SECRETS_FILE}..."
+      log.white "Changes from bitwarden detected, replacing ${SECRETS_FILE} ..."
       mv -f ${TEMP_SECRETS_FILE} ${SECRETS_FILE}
       chmod go-wrx ${SECRETS_FILE}
     fi
 
-    log.white "Generating secret files from notes..."
+    log.white "Generating secret files from notes ..."
     generate_secret_files
     log.white "Secrets files created."
   else
@@ -282,7 +276,7 @@ while true; do
     log.red "--------------------------------------"
     log.red "Ensure that you have:"
     log.red "  - At least 1 secret in your organisation ${BW_ORGANIZATION}"
-    log.red "  - Bitwarden is started when using the Bitwarden add-on"
+    log.red "  - Bitwarden is started when using the bitwarden-secrets docker container"
     log.red "--------------------------------------"
   fi
 
@@ -294,7 +288,7 @@ while true; do
   sleep "${REPEAT_INTERVAL}"
   login_check
 
-  log.white "Syncing Bitwarden vault..."
+  log.white "Syncing bitwarden vault..."
   bw sync &>/dev/null
   log.white "Bitwarden vault synced at: $(bw sync --last)"
 done
